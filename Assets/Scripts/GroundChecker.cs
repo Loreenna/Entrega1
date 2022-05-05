24647841f8efe2e775cbf85ae79f6e6a0f6749bc
Loreenna <95858403+Loreenna@users.noreply.github.com>
2022-05-05 12:33:30 +0200

    Entrega1

---
+++ Assets/Scripts/GroundChecker.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrounChecker : MonoBehaviour
+{
+    public Player _player;
+
+    void Awake()
+    {
+        _player = GameObject.Find("Atenea").GetComponent<Player>();
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        _player.isGrounded = true;
+        _player._animator.SetBool("Jumping", false);
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        _player.isGrounded = false;
+    }
+}
+
